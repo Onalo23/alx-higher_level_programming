7daa88db2cb63cc90c3b275d653cf7d619168787
Onalo23 <charlesarinze7@gmail.com>
2023-08-29 05:26:16 -0700

    mandatory Tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define a class Square"""
+
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, size):
+        """initialise a new Square
+
+        Arguments:
+            size-int: the size of the new square
+        """
+        self.__size = size
